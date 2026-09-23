7dab87a84736dfe3bd1a53712ad4c908bbe44310
dhrumil <dhrumilshivam@gmail.com>
2026-09-23 21:02:53 +0530

    Exercise done!

---
+++ 02_Day_Variables_builtin_functions/exercise.py
@@ -0,0 +1,32 @@
+# Day 2:30 Days of pthon programming
+f_name = 'dhru'
+l_name = 'bha'
+full_name = f_name+l_name
+country = 5
+city = 6
+age = 25
+year = 2026
+is_marrid = False
+is_true = 'no'
+is_light = 'yes'
+a,b,c = 1,2,3
+
+# level 2
+print(type(f_name))
+print(len(f_name))
+print(len(f_name) > len(l_name))
+num1 = 5
+num2 = 4
+total = num1+num2
+diff = num2-num1
+prod = num2*num1
+divv = num2/num1
+mod = num1%num2
+print(num2**num1)
+floor_division = num2//num1
+radius = 30
+area = (22/7)*(radius**2)
+perimeter = 2*22/7*radius
+radi_in = int(input('enter the radius: '))
+print(f'Area = {(22/7)*(radi_in**2)} M sqr')
+print(help(len))
